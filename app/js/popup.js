2071898c55037c06b62a8219c0304503f75eb28d
Vince Chan <vince.yp.chan@gmail.com>
2016-05-25 02:28:09 -0700

    feat: Add thumbnails to popup

--- app/js/popup.js
+++ app/js/popup.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     createBootswatchSelect();
+    createBootswatchThumbnails();
     
     // When apply button is clicked
     document.getElementById("apply-button").addEventListener("click", function(){
@@ -37,10 +38,48 @@ function createBootswatchSelect() {
         var theme = themes[i];
         var option = document.createElement("option");
         option.value = theme.name;
-        option.text = theme.name + " | " + theme.description;        
+        option.text = theme.name + " | " + theme.description;   
         if (theme.name == userThemeName) {
             option.selected = true;
         }
         selectList.appendChild(option);        
+    }
+}
+
+
+/**
+ * Create the thumbnails
+ */
+function createBootswatchThumbnails() {
+    var themes = chrome.extension.getBackgroundPage().bootswatchThemes.themes;
+    var container = document.getElementById("bootswatch-thumbnail-container");
+    
+    //Create and append the thumbnail images
+    for (var i = 0; i < themes.length; i++) {
+        (function() {
+            var theme, div, a, img;
+            theme = themes[i];
+            
+            div = document.createElement("div");
+            div.className = "col-xs-3";
+            
+            a = document.createElement("a");
+            a.className="thumbnail";
+            a.href = "#";    
+            
+            img = document.createElement("img");
+            img.className = "img-responsive";
+            img.src = "img/" + theme.name + ".png";
+            
+            container.appendChild(div);
+            div.appendChild(a);
+            a.appendChild(img);
+            
+            // change the select list selected item when a thumbnail is clicked on.
+            a.addEventListener("click", function(){
+                console.log(theme.name);
+                document.querySelector('#bootswatch-theme-select [value="' + theme.name + '"]').selected = true;
+            });             
+        }());  
     }
 }
